4f868fe0b1a3693f22413a72247d2a1388a0162f
daniel-skliphosovsky <daniel.skliphosovsky@gmail.com>
2025-05-21 19:24:03 +0300

    Optimized downloaders settings getting methods

--- src/DropLoaderApp/Downloaders/DownloaderConfigHandler.cs
+++ src/DropLoaderApp/Downloaders/DownloaderConfigHandler.cs
@@ -1,25 +1,14 @@
-﻿using System.Text.Json;
-using System.Text.Json.Serialization;
-using CryptographerLib;
+﻿using CryptographerLib;
+using DownloadersConfig;
 
 namespace DropLoaderApp.Downloaders
 {
 	public class DownloaderConfigHandler
 	{
-        private static string _jsonPath = "/Users/daniel.skliphosovsky/Downloads/DownloaderConfig/config.json";
-
         public static string GetSoundcloudClientId()
         {
-            DownloaderConfig config = JsonSerializer.Deserialize<DownloaderConfig>(File.ReadAllText(_jsonPath));
-            return Cryptographer.Decrypt(config.SoundcloudClientId);
+            return Cryptographer.Decrypt(DownloadingSettings.SoundcloudClientId);
         }
 	}
-
-    public class DownloaderConfig
-    {
-        [JsonPropertyName("SoundcloudClientId")]
-        public string SoundcloudClientId { get; set; }
-    }
-
 }
 
