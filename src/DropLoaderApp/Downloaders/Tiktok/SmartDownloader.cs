c3c1c7357178da206e492b59b0f03c6b546099f4
daniel-skliphosovsky <daniel.skliphosovsky@gmail.com>
2025-05-29 13:28:55 +0300

    TikTokDownloader added
    - Add TikTokExplode library
    - Add tiktok download functions

---
+++ src/DropLoaderApp/Downloaders/Tiktok/SmartDownloader.cs
@@ -0,0 +1,24 @@
+﻿using TikTokExplode.Publications;
+
+namespace DropLoaderApp.Downloaders
+{
+    public partial class DownloadHandler
+    {
+        private async Task SmartDownloadFromTikTok()
+        {
+            switch (await PublicationClient.GetPublicationType(DownloadLink))
+            {
+                case PublicationClient.PublicationType.Images:
+                    await DownloadImagesFromTikTok();
+                    break;
+                case PublicationClient.PublicationType.Video:
+                    await DownloadVideoFromTikTok();
+                    break;
+                case PublicationClient.PublicationType.Unknown:
+                default:
+                    DownloadingError("Incorrect URL");
+                    return;
+            }
+        }
+    }
+}
---
+++ src/DropLoaderApp/Downloaders/Tiktok/VideoDownloader.cs
@@ -0,0 +1,36 @@
+﻿using TikTokExplode;
+using TikTokExplode.Publications.Videos;
+
+namespace DropLoaderApp.Downloaders
+{
+    public partial class DownloadHandler
+    {
+        private async Task DownloadVideoFromTikTok()
+        {
+            try
+            {
+                DownloadingStarted();
+
+                TikTokClient TikTok = new TikTokClient();
+
+                Progress<double> progress = new Progress<double>(procent =>
+                {
+                    if (popup.BindingContext is ViewModels.DownloadingProgressViewModel downloadingViewModel)
+                    {
+                        downloadingViewModel.DownloadingProgress = (float)procent;
+                    }
+                });
+
+                Video video = await TikTok.Publications.Videos.GetAsync(DownloadLink);
+
+                await TikTok.DownloadVideoAsync(video, DownloadPath, progress: progress);
+                
+                DownloadingFinished();
+            }
+            catch (Exception exception)
+            {
+                DownloadingError(exception.Message);
+            }
+        }
+    }
+}
