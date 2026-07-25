7800e2715ed63e51b5c8958b1c55ea147083ad4b
daniel-skliphosovsky <daniel.skliphosovsky@gmail.com>
2026-07-25 16:30:12 +0300

    fix: include TikTokExplode.dll in repo, simplify CI

--- build-lib.ps1
+++
@@ -1,9 +0,0 @@
-# Build TikTokExplode as DLL and copy to lib folder
-$ErrorActionPreference = "Stop"
-Set-Location $PSScriptRoot
-New-Item -ItemType Directory -Force -Path lib
-
-dotnet publish "../TikTokExplode/src/TikTokExplode/TikTokExplode.csproj" --configuration Release --output "./lib"
-
-Write-Host "TikTokExplode.dll copied to lib/"
-Get-ChildItem lib/TikTokExplode.dll
