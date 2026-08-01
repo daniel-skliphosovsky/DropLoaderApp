825adc7e7906bb30bd4a83023c97f004f643b9e6
daniel-skliphosovsky <daniel.skliphosovsky@gmail.com>
2026-08-01 14:05:01 +0300

    fix: rename maccatalyst platform namespace to avoid App class collision

--- src/DropLoaderApp.App/Platforms/MacCatalyst/AppDelegate.cs
+++ src/DropLoaderApp.App/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,6 +1,6 @@
 ﻿using Foundation;
 
-namespace DropLoaderApp.App;
+namespace DropLoaderApp.MacCatalyst;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
--- src/DropLoaderApp.App/Platforms/MacCatalyst/Program.cs
+++ src/DropLoaderApp.App/Platforms/MacCatalyst/Program.cs
@@ -1,7 +1,7 @@
 ﻿using ObjCRuntime;
 using UIKit;
 
-namespace DropLoaderApp.App;
+namespace DropLoaderApp.MacCatalyst;
 
 public class Program
 {
